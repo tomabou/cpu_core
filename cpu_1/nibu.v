e92c6b25f4b3e38f3ec07ab172acecdfdcc1fe98
tomabou <kazu.8128.9.6@gmail.com>
2019-08-21 19:41:58 +0900

    success compile

--- cpu_1/nibu.v
+++ cpu_1/nibu.v
@@ -3,7 +3,7 @@ module nibu (clk,show);
     output [9:0] show;
 
     reg [31:0] pc = 32'b0;
-    wire [31:0] instruction;
+    wire [31:0] inst;
     wire [31:0] write_data;
     wire [31:0] read_data1;
     wire [31:0] read_data2;
@@ -11,20 +11,18 @@ module nibu (clk,show);
 
     wire reg_write_ctl;
 
-    assign show = instruction[9:0];
+    assign show = inst[9:0];
 
-    inst_memory im1(clk,pc,instruction);
-
+    inst_memory im1(clk,pc,inst);
     registers regs1(
         clk,
-        instruction[19:15],
-        instruction[24:20],
-        instruction[11:7],
+        inst[19:15],
+        inst[24:20],
+        inst[11:7],
         write_data,
-        reg_write_ctl,
-        read_data1;
-        read_data2;
-    )
+        read_data1,
+        read_data2,
+        reg_write_ctl);
 
     always @ (posedge clk) begin
         pc <= pc + 32'b100;
