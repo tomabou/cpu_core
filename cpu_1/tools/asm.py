6d7d859eb36135405756d2b1fb3694f302308395
tomabou <kazu.8128.9.6@gmail.com>
2019-08-25 11:44:50 +0900

    write mif file

--- cpu_1/tools/asm.py
+++ cpu_1/tools/asm.py
@@ -1,10 +1,14 @@
 import itertools
 import sys
+from typing import List
 
 
 def op_imm(op, rd, rs1, imm):
+    rd = int(rd[1:])
+    rs1 = int(rs1[1:])
+    imm = int(imm)
     op_imm_code = 0b0010011
-    if op == 'add':
+    if op == 'addi':
         func3 = 0b000
     x = imm * (2**20) + rs1 * (2**15) + func3 * \
         (2**12) + rd * (2**7) + op_imm_code
@@ -12,6 +16,9 @@ def op_imm(op, rd, rs1, imm):
 
 
 def op(op, rd, rs1, rs2):
+    rd = int(rd[1:])
+    rs1 = int(rs1[1:])
+    rs2 = int(rs2[1:])
     op_imm_code = 0b0110011
     if op == 'add':
         func3 = 0b000
@@ -23,6 +30,16 @@ def op(op, rd, rs1, rs2):
         func3 * (2**12) + rd * (2**7) + op_imm_code
 
     return x
+
+
+def decode_op(tks):
+    OP = ['add', 'slt', 'sltu', 'and', 'or', 'xor', 'sll', 'srl', 'sub', 'sra']
+    if tks[0] in OP:
+        return op(tks[0], tks[1], tks[2], tks[3])
+    OP_IMM = ['addi', 'slti', 'sltiu', 'xori', 'ori', 'andi']
+    if tks[0] in OP_IMM:
+        return op_imm(tks[0], tks[1], tks[2], tks[3])
+    return 0
 
 
 def rename_register(tk):
@@ -104,16 +121,23 @@ def create(content):
     content = map(tokens, content)
     content = list(itertools.chain.from_iterable(map(repeate_nop, content)))
     content = map(pseudoinst, content)
-    content = map(lambda tks: list(map(rename_register, tks)), content)
+    content = list(map(lambda tks: list(map(rename_register, tks)), content))
+    print(list(content))
+    content = list(map(decode_op, content))
     return content
+
+
+def create_mif(content: List[int], filename):
+    print(filename)
+    with open(filename, 'w') as file:
+        file.write('Your text goes here')
 
 
 def main(filename):
     data = open(filename, 'r')
     content = data.read()
     res = create(content)
-    for line in res:
-        print(line)
+    create_mif(res, filename[:-1]+'mif')
     data.close()
 
 
