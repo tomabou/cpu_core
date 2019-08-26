8c6a3753a92dcecde3c79dd23361e5f62ad20415
tomabou <kazu.8128.9.6@gmail.com>
2019-08-26 10:10:16 +0900

    add immgen

--- cpu_1/modules/immgen.v
+++ cpu_1/modules/immgen.v
@@ -5,6 +5,8 @@ module immgen(inst,imm);
     always @ (*) begin
         case(inst[6:2])
             5'b00100: imm <= {{21{inst[31]}},inst[30:20]}; //op_imm
+            5'b00000: imm <= {{21{inst[31]}},inst[30:20]}; //load
+            5'b01000: imm <= {{21{inst[31]}},inst[30:25],inst[11:7]};//store
             5'b11011: imm <= {{12{inst[31]}},inst[19:12],inst[20],inst[30:21],1'b0};//JAL
             5'b11000: imm <= {{20{inst[31]}},inst[7],inst[30:25],inst[11:8],1'b0};//BRANCH
             default: imm <= 32'b0;
