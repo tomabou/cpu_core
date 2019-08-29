338a49d8de86e6791e102091925070dfdc8b24d8
tomabou <kazu.8128.9.6@gmail.com>
2019-08-29 23:57:39 +0900

    add core clk

--- cpu_1/akari.v
+++ cpu_1/akari.v
@@ -9,6 +9,7 @@ module akari(clk,rxd,segment7_1,segment7_2,segment7_3,segment7_4,segment7_5,segm
     output [6:0] segment7_6;
     output txd;
     wire slow_clk;
+    wire core_clk;
     wire [9:0] show;
 
     wire [7:0] uart_in;
@@ -18,8 +19,9 @@ module akari(clk,rxd,segment7_1,segment7_2,segment7_3,segment7_4,segment7_5,segm
     wire uart_empty;
 
     pll_slow pll_slow1(clk,slow_clk);
+    pll_core pll_core1(clk,core_clk);
     nibu nibu1(
-        clk,
+        core_clk,
         show,
         segment7_1,
         segment7_2,
@@ -34,7 +36,7 @@ module akari(clk,rxd,segment7_1,segment7_2,segment7_3,segment7_4,segment7_5,segm
         uart_wrreq);
 
     uart uart1(slow_clk,rxd,txd,
-        clk,
+        core_clk,
         uart_wrreq,
         uart_out,
         uart_rdreq,
