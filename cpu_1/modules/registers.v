5773c335a6db9bb3001dbe5939a3744016f80909
tomabou <kazu.8128.9.6@gmail.com>
2019-09-04 00:18:31 +0900

    fast register

--- cpu_1/modules/registers.v
+++ cpu_1/modules/registers.v
@@ -12,8 +12,15 @@ module registers(clk,rs1i,rs2i,rdi,write_data,read_data1,read_data2,reg_write);
 
 
     always @(posedge clk) begin
-        read_data1 <= (rs1i == 4'b0) ? 32'b0 : regs[rs1i];
-        read_data2 <= (rs2i == 4'b0) ? 32'b0 : regs[rs2i];
+        if (rs1i == rdi)
+            read_data1 <= write_data;
+        else
+            read_data1 <= (rs1i == 4'b0) ? 32'b0 : regs[rs1i];
+        
+        if (rs2i == rdi)
+            read_data2 <= write_data;
+        else
+            read_data2 <= (rs2i == 4'b0) ? 32'b0 : regs[rs2i];
         if (reg_write == 1'b1) 
             regs[rdi] <= write_data;
     end
