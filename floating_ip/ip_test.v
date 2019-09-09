114168be4cbf86c06c4ad7c31ae106c49968311f
tomabou <kazu.8128.9.6@gmail.com>
2019-09-09 11:52:12 +0900

    add test

--- floating_ip/ip_test.v
+++ floating_ip/ip_test.v
@@ -18,15 +18,18 @@ module ip_test (
 
     reg [3:0] count = 4'b0;
 
-    wire [31:0] src ;
+    wire [31:0] src1 ;
+    wire [31:0] src2 ;
     wire [31:0] res;
 
-    assign src = (count == 4'b1) ? 32'b010000001_10010011001100110011001 : 32'b0;
+    assign src1 = (count == 4'b1) ? 32'b010000001_10010011001100110011001 : 32'b0;
+    assign src2 = (count == 4'b1) ? 32'b010000001_10010011001100110011001 : 32'b0;
 
-    cvt_ftoi cvt_ftoi1(clk,1'b0,src,res);
+    //cvt_ftoi cvt_ftoi1(clk,1'b0,src,res);
+    add add1(clk,1'b0,src1,src2,res);
 
     seg7 seg7_1(count,segment7_1);
-    seg7 seg7_2(src[3:0],segment7_2);
+    seg7 seg7_2(src1[3:0],segment7_2);
     seg7 seg7_3(res[3:0],segment7_3);
     seg7 seg7_4(res[7:4],segment7_4);
     seg7 seg7_5(res[27:24],segment7_5);
