801110ba86a8b64e084491acde4197b750464bf0
tomabo <kazu.8128.9.6@gmail.com>
2019-09-13 19:11:19 +0900

    add nibuio.h

--- cfile/fibo/fibo.s
+++ cfile/fibo/fibo.s
@@ -2,11 +2,58 @@
 	.option nopic
 	.text
 	.align	2
+	.globl	nibu_input_func
+	.type	nibu_input_func, @function
+nibu_input_func:
+#APP
+# 6 "cfile/fibo/../header/nibuio.h" 1
+	lw    a0, 4(zero);
+# 0 "" 2
+#NO_APP
+	ret
+	.size	nibu_input_func, .-nibu_input_func
+	.align	2
+	.globl	nibu_input
+	.type	nibu_input, @function
+nibu_input:
+	li	a5,-1
+.L4:
+#APP
+# 6 "cfile/fibo/../header/nibuio.h" 1
+	lw    a0, 4(zero);
+# 0 "" 2
+#NO_APP
+	beq	a0,a5,.L4
+	ret
+	.size	nibu_input, .-nibu_input
+	.align	2
+	.globl	nibu_output
+	.type	nibu_output, @function
+nibu_output:
+#APP
+# 19 "cfile/fibo/../header/nibuio.h" 1
+	sw      a0, 4(zero);
+# 0 "" 2
+#NO_APP
+	ret
+	.size	nibu_output, .-nibu_output
+	.align	2
+	.globl	nibu_show
+	.type	nibu_show, @function
+nibu_show:
+#APP
+# 24 "cfile/fibo/../header/nibuio.h" 1
+	sw      a0, 0(zero);
+# 0 "" 2
+#NO_APP
+	ret
+	.size	nibu_show, .-nibu_show
+	.align	2
 	.globl	fibo
 	.type	fibo, @function
 fibo:
 	li	a5,1
-	ble	a0,a5,.L10
+	ble	a0,a5,.L16
 	addi	sp,sp,-16
 	addi	a5,a0,-2
 	sw	s2,0(sp)
@@ -18,12 +65,12 @@ fibo:
 	addi	s0,a0,-1
 	sub	s2,s2,a5
 	li	s1,0
-.L3:
+.L10:
 	mv	a0,s0
 	call	fibo
 	addi	s0,s0,-2
 	add	s1,s1,a0
-	bne	s0,s2,.L3
+	bne	s0,s2,.L10
 	lw	ra,12(sp)
 	lw	s0,8(sp)
 	lw	s2,0(sp)
@@ -31,7 +78,7 @@ fibo:
 	lw	s1,4(sp)
 	addi	sp,sp,16
 	jr	ra
-.L10:
+.L16:
 	li	a0,1
 	ret
 	.size	fibo, .-fibo
@@ -41,29 +88,40 @@ fibo:
 	.type	main, @function
 main:
 	addi	sp,sp,-16
-	sw	s1,4(sp)
 	sw	ra,12(sp)
 	sw	s0,8(sp)
+	sw	s1,4(sp)
 	sw	s2,0(sp)
-	call	nibu_input
+	li	a5,-1
+.L18:
+#APP
+# 6 "cfile/fibo/../header/nibuio.h" 1
+	lw    a0, 4(zero);
+# 0 "" 2
+#NO_APP
+	beq	a0,a5,.L18
 	li	a5,1
 	li	s1,0
-	ble	a0,a5,.L13
+	ble	a0,a5,.L20
 	addi	a5,a0,-2
 	addi	s2,a0,-3
 	andi	a5,a5,-2
 	addi	s0,a0,-1
 	sub	s2,s2,a5
 	li	s1,0
-.L14:
+.L21:
 	mv	a0,s0
 	call	fibo
 	addi	s0,s0,-2
 	add	s1,s1,a0
-	bne	s0,s2,.L14
-.L13:
-	addi	a0,s1,1
-	call	nibu_output
+	bne	s0,s2,.L21
+.L20:
+	addi	s1,s1,1
+#APP
+# 19 "cfile/fibo/../header/nibuio.h" 1
+	sw      s1, 4(zero);
+# 0 "" 2
+#NO_APP
 	lw	ra,12(sp)
 	lw	s0,8(sp)
 	lw	s1,4(sp)
--- cfile/header/nibuio.h
+++ cfile/header/nibuio.h
@@ -1,9 +1,28 @@
 #ifndef NIBUIO_H
 #define NIBUIO_H
 
-int nibu_input();
-void nibu_output();
-void nibu_show();
+int nibu_input_func(void) {
+    int ret;
+    asm volatile("lw    %0, 4(zero);" : "=r"(ret));
+    return ret;
+}
+
+int nibu_input() {
+    int x = -1;
+    while (x == -1) {
+        x = nibu_input_func();
+    }
+    return x;
+}
+
+void nibu_output(int n) {
+    asm volatile("sw      %0, 4(zero);" : : "r"(n));
+    return;
+}
+
+void nibu_show(int n) {
+    asm volatile("sw      %0, 0(zero);" : : "r"(n));
+    return;
+}
 
 #endif
-
