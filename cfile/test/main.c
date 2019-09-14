ba15bbd8e027215a30c111ea878fc0040fe39cc5
tomabo <kazu.8128.9.6@gmail.com>
2019-09-14 13:47:45 +0900

    add smaple

--- cfile/test/main.c
+++ cfile/test/main.c
@@ -1,2 +1,9 @@
+#include "../header/nibuio.h"
 
-
+int main() {
+    while (1) {
+        int x = nibu_input();
+        nibu_output(x);
+    }
+    return 0;
+}
---
+++ cfile/test/main.s
@@ -0,0 +1,90 @@
+	.file	"main.c"
+	.option nopic
+	.text
+	.align	2
+	.globl	nibu_input_func
+	.type	nibu_input_func, @function
+nibu_input_func:
+#APP
+# 8 "cfile/test/../header/nibuio.h" 1
+	lw    a0, 4(zero);
+# 0 "" 2
+#NO_APP
+	ret
+	.size	nibu_input_func, .-nibu_input_func
+	.align	2
+	.globl	nibu_input
+	.type	nibu_input, @function
+nibu_input:
+	li	a5,-1
+.L4:
+#APP
+# 8 "cfile/test/../header/nibuio.h" 1
+	lw    a0, 4(zero);
+# 0 "" 2
+#NO_APP
+	beq	a0,a5,.L4
+	ret
+	.size	nibu_input, .-nibu_input
+	.align	2
+	.globl	nibu_output
+	.type	nibu_output, @function
+nibu_output:
+#APP
+# 21 "cfile/test/../header/nibuio.h" 1
+	sw      a0, 4(zero);
+# 0 "" 2
+#NO_APP
+	ret
+	.size	nibu_output, .-nibu_output
+	.align	2
+	.globl	nibu_show
+	.type	nibu_show, @function
+nibu_show:
+#APP
+# 26 "cfile/test/../header/nibuio.h" 1
+	sw      a0, 0(zero);
+# 0 "" 2
+#NO_APP
+	ret
+	.size	nibu_show, .-nibu_show
+	.align	2
+	.globl	print_string
+	.type	print_string, @function
+print_string:
+	lbu	a5,0(a0)
+	beq	a5,zero,.L8
+.L10:
+#APP
+# 21 "cfile/test/../header/nibuio.h" 1
+	sw      a5, 4(zero);
+# 0 "" 2
+#NO_APP
+	lbu	a5,1(a0)
+	addi	a0,a0,1
+	bne	a5,zero,.L10
+.L8:
+	ret
+	.size	print_string, .-print_string
+	.section	.text.startup,"ax",@progbits
+	.align	2
+	.globl	main
+	.type	main, @function
+main:
+	li	a4,-1
+.L16:
+#APP
+# 8 "cfile/test/../header/nibuio.h" 1
+	lw    a5, 4(zero);
+# 0 "" 2
+#NO_APP
+	beq	a5,a4,.L16
+#APP
+# 21 "cfile/test/../header/nibuio.h" 1
+	sw      a5, 4(zero);
+# 0 "" 2
+#NO_APP
+	j	.L16
+	.size	main, .-main
+	.ident	"GCC: (GNU) 9.2.0"
+	.section	.note.GNU-stack,"",@progbits
