9b146bd3412f02ccfcce06459f0b79f70601fa53
tomabo <kazu.8128.9.6@gmail.com>
2019-09-20 23:58:13 +0900

    add mul test file

---
+++ cfile/test/mul.c
@@ -0,0 +1,11 @@
+#include "../header/nibuio.h"
+
+int main() {
+    while (1) {
+        int x = nibu_input();
+        int y = nibu_input();
+        int a = (x - '0') + (x - '0') * (y - '0');
+        nibu_show(a);
+    }
+    return 0;
+}
