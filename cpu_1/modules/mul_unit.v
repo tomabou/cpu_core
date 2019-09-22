bfc048604ba61687d692e13a17fdd21f141ec799
tomabou <kazu.8128.9.6@gmail.com>
2019-09-22 13:29:13 +0900

    fix mulh

--- cpu_1/modules/mul_unit.v
+++ cpu_1/modules/mul_unit.v
@@ -12,6 +12,8 @@ module mul_unit (clk,a,b,funct3,res);
     wire [39:0] ope1;
     wire [39:0] ope2;
     wire [79:0] q;
+    wire ope1_top;
+    wire ope2_top;
 
     reg is_upperbit_buf1;
     reg is_upperbit_buf2;
@@ -22,8 +24,11 @@ module mul_unit (clk,a,b,funct3,res);
     assign is_hu = (funct3 == 3'b011);
     assign is_upperbit = is_h | is_hu | is_hsu;
 
-    assign ope1 = {{8{is_h | is_hsu}},a};
-    assign ope2 = {{8{is_h}},b};
+
+    assign ope1_top = (is_h | is_hsu) & a[31];
+    assign ope2_top = is_h  & b[31];
+    assign ope1 = {{8{ope1_top}},a};
+    assign ope2 = {{8{ope2_top}},b};
 
     assign res = is_upperbit_buf2 ? q[63:32] 
                : q[31:0];
