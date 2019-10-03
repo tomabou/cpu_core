d18e2796a5a5a76e82031fb582e25c8bf99586f3
tomabo <kazu.8128.9.6@gmail.com>
2019-10-03 11:43:25 +0900

    without -1 check

--- src/uart/uart_test_nonop.s
+++ src/uart/uart_test_nonop.s
@@ -2,8 +2,8 @@ main:
     addi    s1, zero, -1
 .LBB0_1:                              
     lw      t0, 4(zero)
-    beq     t0, s1, .LBB0_1
-    addi    t0, t0, 1
+;    beq     t0, s1, .LBB0_1
+;    addi    t0, t0, 1
     sw      t0, 0(zero)
     sw      t0, 4(zero)
     j       .LBB0_1
